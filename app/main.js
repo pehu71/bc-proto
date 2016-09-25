b73c32dd6e2e83be1ecc3eab9c1cd160a958ca30
pehu71 <humplikpetr@gmail.com>
2016-09-26 01:35:10 +0200

    first working AOT version

--- app/main.js
+++ app/main.js
@@ -1,6 +1,4 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 //# sourceMappingURL=main.js.map
