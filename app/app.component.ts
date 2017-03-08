869bd626b529098a235311507678e0fe873c88bf
pehu71 <pehu@pehu.info>
2017-03-08 09:13:33 +0100

    restruct to dev/prod scenario

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/tpl/app.component.html'
 })
 export class AppComponent { }
--- app/index.component.ts
+++ app/index.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    templateUrl: 'app/index.html'
+    templateUrl: 'app/tpl/index.html'
 })
 
 export class IndexComponent {}
