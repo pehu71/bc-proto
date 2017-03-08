19eca86b02f2fb2eb85fe2c5d772fcc97d140676
pehu71 <pehu@pehu.info>
2017-03-08 13:34:12 +0100

    feature module 2

--- prod/routes-aot.ts
+++
@@ -1,7 +0,0 @@
-import {IndexComponent} from "./index.component";
-import {Routes} from "@angular/router";
-
-export const routes: Routes = [
-    {path: '', component: IndexComponent},
-    {path: 'feature', loadChildren: 'app/feature.module#FeatureModule'}
-];
