41c37a10332e73184f8d6e0e261e8dab6a1710c9
pehu71 <pehu@pehu.info>
2017-03-08 10:52:31 +0100

    feature module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,14 @@
-import {NgModule}      from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
-import {AppComponent}   from './app.component';
+import {AppComponent} from './app.component';
 import {IndexComponent} from "./index.component";
 
 @NgModule({
     imports: [BrowserModule,
         RouterModule.forRoot([
-            {path: '', component: IndexComponent}
+            {path: '', component: IndexComponent},
+            {path: 'feature', loadChildren: 'app/feature.module#FeatureModule'}
         ])
     ],
     declarations: [AppComponent, IndexComponent],
