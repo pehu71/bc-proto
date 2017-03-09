eeba246c9d942939e46f7b61e2bf5c8378576b46
pehu71 <pehu@pehu.info>
2017-03-09 13:53:05 +0100

    prepared for webpack

--- script-aot-src/app.routes.ts
+++ script-aot-src/app.routes.ts
@@ -3,5 +3,5 @@ import {Routes} from "@angular/router";
 
 export const routes: Routes = [
     {path: '', component: IndexComponent},
-    {path: 'feature', loadChildren: 'app/feature.module#FeatureModule'}
+    {path: 'feature', loadChildren: './feature.module#FeatureModule'}
 ];
